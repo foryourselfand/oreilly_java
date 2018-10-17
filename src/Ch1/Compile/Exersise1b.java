59749e89a6a7be7eb54d2b3b12983511022e3810
foryourselfand <foryourselfand@gmail.com>
2018-10-18 00:24:02 +0300

    Charapter 1 (Almoust) Done

---
+++ src/Ch1/Compile/Exersise1b.java
@@ -0,0 +1,24 @@
+package Ch1.Compile;
+
+//public class Exersise1b {
+public class Exersise1b {
+    //    public static void main(String[] args) {
+    public static void main(String[] args) {
+        /*
+        int x = 5;
+
+        while (x > 1){
+            x = x - 1;
+            if (x < 3)
+                System.out.println("Маленький х");
+        }
+         */
+
+        int x = 1;
+        while (x < 10) {
+            if (x > 3)
+                System.out.println("большой х");
+            x = x + 1;
+        }
+    }
+}
---
+++ src/Ch1/PoolPuzzleOne.java
@@ -0,0 +1,21 @@
+package Ch1;
+
+public class PoolPuzzleOne {
+    public static void main(String[] args) {
+        int x = 0;
+
+        while (x < 4) {
+            if (x < 1) {
+
+            }
+
+
+            if (x == 1) {
+
+            }
+
+            System.out.println();
+
+        }
+    }
+}
