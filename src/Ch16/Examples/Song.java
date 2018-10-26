864abbf76672cfed514e710b64c6041f47b0cbe7
foryourselfand <foryourselfand@gmail.com>
2018-10-26 13:18:08 +0300

    Second (or third?) version. Added class Song

--- src/Ch16/Examples/Song.java
+++ src/Ch16/Examples/Song.java
@@ -1,5 +1,36 @@
 package Ch16.Examples;
 
 public class Song {
+    String title;
+    String atrist;
+    String rating;
+    String bpm;
 
+    public Song(String title, String atrist, String rating, String bpm) {
+        this.title = title;
+        this.atrist = atrist;
+        this.rating = rating;
+        this.bpm = bpm;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAtrist() {
+        return atrist;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getBpm() {
+        return bpm;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
 }
